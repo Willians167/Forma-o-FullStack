d19677fb338622a24205cee75407b305d2dffcab
Willians <prwillians.costa@gmail.com>
2024-05-27 07:14:00 -0300

    Adiciona Arquivos de aulas

---
+++ Poo-sharp/Payments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+namespace Payments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+
+                Console.WriteLine("Hello World!");
+            }
+        }
+class Pagamento
+{
+    //Propriedades
+DateTime Vencimeto;
+    //Métodos
+void Pagar()
+{
+   ConsultarSaldoDoCartão();
+}    
+ private void ConsultarSaldoDoCartão()
+{
+
+}
+
+}
+
+
+    }
+}
